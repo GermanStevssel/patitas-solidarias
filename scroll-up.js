b3b9d9137ece9eef5f088498c741a4a25c753ee8
GermanStevssel <germancstevssel@gmail.com>
2021-07-04 02:07:03 -0300

    changing up button

--- scroll-up.js
+++ scroll-up.js
@@ -1,13 +1,4 @@
 $(document).ready(function () {
-	$(".up").click(function () {
-		$("body, html").animate(
-			{
-				scrollTop: "0px",
-			},
-			300
-		);
-	});
-
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 300) {
       $(".up").fadeIn(300);
